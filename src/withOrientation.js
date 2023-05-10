b4dc1cc468f4b893f71081de467921c9c97a8201
dreleks <46107277+dreleks@users.noreply.github.com>
2023-05-10 15:04:58 +0300

    Update withOrientation.js

--- src/withOrientation.js
+++ src/withOrientation.js
@@ -18,7 +18,7 @@ export default function(WrappedComponent) {
     }
 
     componentWillUnmount() {
-      this.dimensionsSubscription?.remove();
+      this.dimensionsSubscription.remove();
     }
 
     handleOrientationChange = ({ window }) => {
